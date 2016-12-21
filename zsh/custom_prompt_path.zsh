3cf7c554baa9d787c2e82316f23a29905b6a3290
Joshua Rieken <joshua@joshuarieken.com>
2016-12-20 19:25:43 -0600

    Use .ywd.local for zsh scripts

--- zsh/custom_prompt_path.zsh
+++ zsh/custom_prompt_path.zsh
@@ -1,4 +1,4 @@
 #Load themes from YWD and from user's custom prompts (themes) in ~/.zsh.prompts
 autoload promptinit
-fpath=($HOME/.ywd/zsh/prezto-themes $HOME/.zsh.prompts $fpath)
+fpath=($HOME/.ywd/zsh/prezto-themes $HOME/.ywd.local/zsh/prompts $fpath)
 promptinit
--- zsh/zzzz_after.zsh
+++ zsh/zzzz_after.zsh
@@ -1,6 +1,6 @@
 # Load any custom after code
 if [ -d $HOME/.zsh.after/ ]; then
-  if [ "$(ls -A $HOME/.zsh.after/)" ]; then
-    for config_file ($HOME/.zsh.after/*.zsh) source $config_file
+  if [ "$(ls -A $HOME/.ywd.local/zsh/after/)" ]; then
+    for config_file ($HOME/.ywd.local/zsh/after/*.zsh) source $config_file
   fi
 fi
