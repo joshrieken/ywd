456458b2ffcf662d71ab5fbe0220ab3432c57af2
Joshua Rieken <joshua@joshuarieken.com>
2016-12-21 11:52:17 -0600

    Look for correct dir

--- zsh/0000_before.zsh
+++ zsh/0000_before.zsh
@@ -1,6 +1,5 @@
 # Load any user customizations prior to load
-#
-if [ -d $HOME/.zsh.before/ ]; then
+if [ -d $HOME/.ywd.local/zsh/before ]; then
   if [ "$(ls -A $HOME/.ywd.local/zsh/before/)" ]; then
     for config_file ($HOME/.ywd.local/zsh/before/*.zsh) source $config_file
   fi
--- zsh/zzzz_after.zsh
+++ zsh/zzzz_after.zsh
@@ -1,5 +1,5 @@
 # Load any custom after code
-if [ -d $HOME/.zsh.after/ ]; then
+if [ -d $HOME/.ywd.local/zsh/after ]; then
   if [ "$(ls -A $HOME/.ywd.local/zsh/after/)" ]; then
     for config_file ($HOME/.ywd.local/zsh/after/*.zsh) source $config_file
   fi
